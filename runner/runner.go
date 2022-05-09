70b47356c51506bb84f4d0118fe272c9112eaf54
ndjordjevic <ndjordjevic@gmail.com>
2022-05-09 08:01:00 +0200

    pool pattern from go in action

--- runner/runner.go
+++ runner/runner.go
@@ -1,6 +1,11 @@
 // Example is provided with help by Gabriel Aszalos.
 // Package runner manages the running and lifetime of a process.
 // It runs a slice of tasks and could be interrupted or timeout
+// The purpose of the runner package is to show how channels can be used to monitor
+// the amount of time a program is running and terminate the program if it runs
+// too long. This pattern is useful when developing a program that will be scheduled
+// to run as a background task process. This could be a program that runs as a cron
+// job, or in a worker-based cloud environment like Iron.io.
 package runner
 
 import (
