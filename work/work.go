311650419c070c7038b94669efb35f64576ea6d1
ndjordjevic <ndjordjevic@gmail.com>
2022-05-09 08:15:38 +0200

    work pattern from go in action

---
+++ work/work.go
@@ -0,0 +1,51 @@
+// Example provided with help from Jason Waldrip.
+// Package work manages a pool of goroutines to perform work.
+// The purpose of the package is to show how you can use an unbuffered channel
+// work to create a pool of goroutines that will perform and control the amount of work that
+// gets done concurrently.
+package work
+
+import "sync"
+
+// Worker must be implemented by types that want to use
+// the work pool.
+type Worker interface {
+	Task()
+}
+
+// Pool provides a pool of goroutines that can execute any Worker
+// tasks that are submitted.
+type Pool struct {
+	work chan Worker
+	wg   sync.WaitGroup
+}
+
+// New creates a new work pool.
+func New(maxGoroutines int) *Pool {
+	p := Pool{
+		work: make(chan Worker),
+	}
+
+	p.wg.Add(maxGoroutines)
+	for i := 0; i < maxGoroutines; i++ {
+		go func() {
+			for w := range p.work {
+				w.Task()
+			}
+			p.wg.Done()
+		}()
+	}
+
+	return &p
+}
+
+// Run submits work to the pool.
+func (p *Pool) Run(w Worker) {
+	p.work <- w
+}
+
+// Shutdown waits for all the goroutines to shutdown.
+func (p *Pool) Shutdown() {
+	close(p.work)
+	p.wg.Wait()
+}
