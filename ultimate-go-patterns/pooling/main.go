00993ba73c1387c5e4f9bd34d798b9f2c6d1381d
ndjordjevic <ndjordjevic@gmail.com>
2022-05-11 08:49:03 +0200

    basic pooling pattern from ultimate go

---
+++ ultimate-go-patterns/pooling/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
+func main() {
+	ch := make(chan string)
+
+	// Good point to start is to have the no of go routines the same as no of cpu cores
+	g := runtime.GOMAXPROCS(0)
+	for c := 0; c < g; c++ {
+		go func(child int) {
+			for d := range ch {
+				fmt.Printf("child %d : recv'd signal : %s\n", child, d)
+			}
+			fmt.Printf("child %d : recv'd shutdown signal\n", child)
+		}(c)
+	}
+
+	const work = 100
+	for w := 0; w < work; w++ {
+		ch <- "data"
+		fmt.Println("parent : sent signal :", w)
+	}
+
+	close(ch)
+	fmt.Println("parent : sent shutdown signal")
+
+	time.Sleep(time.Second)
+	fmt.Println("-------------------------------------------------")
+}
