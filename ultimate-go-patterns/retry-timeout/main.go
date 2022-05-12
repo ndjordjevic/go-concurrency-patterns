cd2d6f17695da2e47f1c6637429b2ec1fe18efc7
ndjordjevic <ndjordjevic@gmail.com>
2022-05-12 11:38:20 +0200

    retry timeout pattern from ultimate go

---
+++ ultimate-go-patterns/retry-timeout/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+)
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	retryTimeout(ctx, time.Second, func(ctx context.Context) error { return errors.New("always fail") })
+}
+
+func retryTimeout(ctx context.Context, retryInterval time.Duration, check func(ctx context.Context) error) {
+
+	for {
+		fmt.Println("perform user check call")
+		if err := check(ctx); err == nil {
+			fmt.Println("work finished successfully")
+			return
+		}
+
+		fmt.Println("check if timeout has expired")
+		if ctx.Err() != nil {
+			fmt.Println("time expired 1 :", ctx.Err())
+			return
+		}
+
+		fmt.Printf("wait %s before trying again\n", retryInterval)
+		t := time.NewTimer(retryInterval)
+
+		select {
+		case <-ctx.Done():
+			fmt.Println("time expired 2 :", ctx.Err())
+			t.Stop()
+			return
+		case <-t.C:
+			fmt.Println("retry again")
+		}
+	}
+}
